fc409ea5fc31a10299a3bfd8353116088ecaf418
koji <koji@lenovo.localdomain>
2017-11-11 17:12:41 +0900

    Update main.py.

--- main.py
+++ main.py
@@ -17,18 +17,11 @@ parser.add_argument('map', type=str, help='name of the map you import')
 parser.add_argument('url', type=str, help='Map Warper URL')
 parser.add_argument('-u', '--user', action='store', type=str, required=True, help='a username')
 parser.add_argument('-p', '--password', action='store', type=str, required=True, help='the password for the username')
-"""
+
 MAP_NAME = parser.parse_args().map
 USER = parser.parse_args().user
 PASSWORD = parser.parse_args().password
 URL = parser.parse_args().url
-
-"""
-MAP_NAME = "gomanbunnoichichikeizu898.jpg"
-USER = "super@example.com"
-PASSWORD = "your_password"
-URL = "http://ec2-52-198-241-210.ap-northeast-1.compute.amazonaws.com:8080"
-
 
 authentication_url = URL + "/u/sign_in.json"
 authentication_url = re.sub(r'//u/sign_in', '/u/sign_in', authentication_url)
